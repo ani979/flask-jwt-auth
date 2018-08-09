e383d7befda711845f7f781251df2445be128e8c
Animesh Shrivastava <ani979@gmail.com>
2018-08-09 22:25:16 +0530

    added cors policy

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app_settings = os.getenv(
     'APP_SETTINGS',
